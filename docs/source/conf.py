aeae1f2d5609b9ad48c4972cc51a002bf31270d5
Shuai Feng <451424498@qq.com>
2023-01-06 21:31:40 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Gehong'
+copyright = '2022, S.Feng'
+author = 'Shuai Feng'
 
 release = '0.1'
 version = '0.1.0'
