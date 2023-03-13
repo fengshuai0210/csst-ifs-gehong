42e34375ccf87f14f9d88dde3f9f992de23a922c
Shuai Feng <451424498@qq.com>
2023-03-13 18:20:39 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/Users/sfeng/Fastro/Code/csst-ifs-gehong/gehong/'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
