f835170e64f20e3e054d65d241b784a28f2278cd
Shuai Feng <451424498@qq.com>
2023-05-02 18:42:22 +0800

    Update config.py

--- gehong/config.py
+++ gehong/config.py
@@ -2,31 +2,28 @@ import numpy as np
 
 class config():
     """
-    
-    """
-    def __init__(self, wave_min = 3500, wave_max = 10000, 
-                 dlam = 1, inst_fwhm = 2.5,
-                 nx = 100, ny = 100, dpix = 0.1):
-        """
-        __init__ _summary_
+    The configuration of spectral modeling. The default value is used for ETC calculation.
 
-        Parameters
-        ----------
-        wave_min : int, optional
-            _description_, by default 3500
-        wave_max : int, optional
-            _description_, by default 10000
-        dlam : int, optional
-            _description_, by default 1
-        inst_fwhm : float, optional
-            _description_, by default 2.5
-        nx : int, optional
-            _description_, by default 100
-        ny : int, optional
-            _description_, by default 100
-        dpix : float, optional
-            _description_, by default 0.1
-        """
+    Parameters
+    ----------
+    wave_min : float, optional
+        Minimum value of wavelength coverage, by default 3500.0A
+    wave_max : float, optional
+        Minimum value of wavelength coverage, by default 10000.0A
+    dlam : float, optional
+        Wavelength width of each spaxel, by default 2.0A
+    inst_fwhm : float, optional
+        Spectral resolution, by default 0.1A
+    nx : int, optional
+        Number of spaxel in a spatial direction, by default 30
+    ny : int, optional
+        Number of spaxel in a spatial direction, by default 30
+    dpix : float, optional
+        Pixel size in the spatial direction, by default 0.2arcsec
+    """
+    def __init__(self, wave_min = 3500.0, wave_max = 10000.0, 
+                 dlam = 2.0, inst_fwhm = 0.1,
+                 nx = 30, ny = 30, dpix = 0.2):
         self.dlam = dlam
         self.wave = np.arange(wave_min, wave_max, dlam)
         self.wave_min = wave_min
