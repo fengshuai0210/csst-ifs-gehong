90959bbb1fc2d7f4e677989b16c3997ced3fcc80
Shuai Feng <451424498@qq.com>
2023-05-03 18:48:31 +0800

    update

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='csst-ifs-gehong',
+    version='0.0.1.4',
+    license='MIT',
+    author="Shuai Feng",
+    author_email='sfeng@hebtu.edu.cn',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://csst-ifs-gehong.readthedocs.io/en/latest/',
+    keywords='CSST-IFS',
+    install_requires=[
+          'astropy',
+      ],
+
+)
