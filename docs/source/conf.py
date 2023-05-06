c4585f2af87fbe202d00fedf5381276030717351
Shuai Feng <451424498@qq.com>
2023-05-06 09:44:58 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, CSST-IFS Team'
 author = 'Shuai Feng'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 
